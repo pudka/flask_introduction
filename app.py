1abc0b20197f5bd675ca9e174b91c31c5eb0b393
pudka <pudgeinvonyx@gmail.com>
2020-07-21 19:24:30 +0200

    Base CRUD completed.

--- app.py
+++ app.py
@@ -1,11 +1,67 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+class Person(db.Model):
+    #__tablename__ = 'person'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Introduction to Flask"
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Person(name=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+    else:
+        tasks = Person.query.order_by(Person.id).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Person.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting a test task."
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task_to_update = Person.query.get_or_404(id)
+
+    if request.method == 'POST':
+        update_content = request.form['content']
+        task_to_update.name = update_content
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was a problem with update a test task."
+
+    else:
+        return render_template('update.html', task=task_to_update)
 
 
 if __name__ == "__main__":
